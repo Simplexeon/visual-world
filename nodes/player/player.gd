50db40a0c9ba12ab53a07b36b25eca257206a65a
Ryan Doyle (RIT Student) <rad8555@rit.edu>
2025-01-14 13:48:19 -0500

    Initial commit

---
+++ nodes/player/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+class_name Player3D
+
+# Parameters
+
+@export var Speed : float = 5.0;
+
+
+
+# Processes
+
+func _input(event: InputEvent) -> void:
+	
+	if(event is InputEventMouseButton):
+		if(event.button_index == 1 and event.pressed):
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
+	
+	if(event.is_action_pressed("ui_cancel")):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE;
+	
+
+
+func _physics_process(delta: float) -> void:
+	# INPUT FROM DEFAULT CHARACTER SCRIPT
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta;
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir : Vector2 = Input.get_vector("left", "right", "forward", "backward");
+	var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized();
+	
+	if direction:
+		velocity.x = direction.x * Speed;
+		velocity.z = direction.z * Speed;
+	else:
+		velocity.x = move_toward(velocity.x, 0, Speed);
+		velocity.z = move_toward(velocity.z, 0, Speed);
+	
+	move_and_slide();
